4aa74d4ad610f4ecd9c219326c58bc445696097e
Abdelhamid El Bouni <elbouni2003@gmail.com>
2024-10-12 19:34:39 +0200

    nav bar set up

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    const [isMenuopen, setMenuOpen] = useState(fasle);
+    const [isSticky, setSticky] = useState(false);
+    
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
